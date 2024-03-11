cf760cbec6b45cbdce106d1ce3cfbcee443c5372
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-03-11 15:59:03 -0300

    mapa fijado

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,13 +17,20 @@ export default class extends Controller {
     })
 
     this.#addMarkersToMap()
+    this.#fitMapToMarkers() // Asegura que el mapa se ajuste a los marcadores.
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+        .setLngLat([marker.lng, marker.lat])
         .addTo(this.map)
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
